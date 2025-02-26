6b2c0e14621fe2a2cd247d391e6bef7da6c613a3
mapins <mariocha82004@gmail.com>
2025-02-26 08:56:12 +0100

    added new exercise

---
+++ ejercicio 1 html/script.js
@@ -0,0 +1,10 @@
+const newUser = {
+    userName : 'Marlon',
+    userAge : 29
+}
+
+
+console.log('hola');
+
+const {userName, userAge} = newUser;
+console.log(newUser);
---
+++ ejercicios js/script.js
@@ -0,0 +1,13 @@
+const newUser = {
+    userName : 'Marlon',
+    userAge : 29,
+    userID : 1542
+}
+
+const {userName, userAge, userID} = newUser;
+
+console.log(newUser);
+
+
+newUser.userName = 'rocio';
+console.log(newUser);
